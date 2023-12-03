9338a1f9c53c4fad92ca78ffbd25910ff178a5e8
laddeeth <ladde@ladde.se>
2023-12-04 00:28:47 +0100

    Lucka 3 avklarad

---
+++ dag3/dag3.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+fs.readFile('dag3/input.txt', (err, textFile) => {
+  if (err) throw err;
+  let lines = textFile.toString().split('\n');
+  let total = 0;
+  for (let x = 0; x < lines.length; x++) {
+    total += parseInt(lines[x]);
+  }
+  let medel = total / lines.length;
+  let totaltGoda = 0;
+  for (let x = 0; x < lines.length; x++) {
+    if (lines[x] == medel) {
+      totaltGoda++;
+    }
+  }
+  console.log(totaltGoda);
+});
